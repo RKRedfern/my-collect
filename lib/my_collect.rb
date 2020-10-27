c289aaf0c57c6306ffb2f274b45bbbac6141d831
RKRedfern <rkredfern@gmail.com>
2020-10-27 09:35:58 -0400

    my_collect.rb

--- lib/my_collect.rb
+++ lib/my_collect.rb
@@ -1,2 +1,36 @@
 
+def my_collect(array)
+    i = 0
+    collect = []
+    while i < array.length
+        collect << yield(array[i])
+        i = i + 1
+    end 
+    collect
+end
+
+
+# def my_each(array)
+#     i = 0 
+#     while i < array.length
+#         yield(array[i])
+#         i = i + 1 
+#     end 
+#     array
+# end
+
+# require 'pry'
+
+# def hello(array)
+#   i = 0
+#   while i < array.length
+#     yield(array[i])
+#     i += 1
+#   end
+# end
+
+
+# hello(["Tim", "Tom", "Jim"]) { |name| puts "Hi, #{name}" }
+
+
 
